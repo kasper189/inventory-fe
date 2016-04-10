8b7b1e81c06ca58717fdc2c685e5ec1b5db1591f
Davide Sanapo <davide.sanapo@gmail.com>
2016-04-10 17:10:07 +0200

    Added possibility to insert new product

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,10 +16,18 @@ angular.module('inventoryFeApp')
         $scope.inventory_list = data;
     });
 
-//    $scope.inventory_list =
-//      [
-//        { item: "Bagnoschiuma", count: 2 },
-//        { item: "Dentifricio", count: 3 }
-//      ]
+    //$scope.addItem = function(item, count) {
+    //  $scope.inventory_list.push( { item: item.toUpperCase(), count: count } );
+    //}
+
+    $scope.addItem = function(item, count)
+    {
+      var newProduct = { item: item.toUpperCase(), count: count }
+      $http.post('http://inventory-backend.herokuapp.com/inventory', newProduct)
+        .success(function (newProduct)
+        {
+          $scope.inventory_list.push(newProduct);
+        });
+    }
 
 }]);
---
+++ app/scripts/helpers.js
@@ -0,0 +1,5 @@
+function ClearFields()
+{
+  document.getElementById("id_item").value = "";
+  document.getElementById("id_count").value = "";
+}
